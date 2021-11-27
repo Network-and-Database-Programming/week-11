379891928c9bea96d8b2b217111fc4264093a444
shanacon <weston123741852@gmail.com>
2021-11-27 22:09:02 +0800

    delete test

--- test.py
+++
@@ -1,7 +0,0 @@
-import MySQLdb
-db=MySQLdb.connect(host="localhost", user="root", password="w123741852", db="filedata")
-cur=db.cursor()
-cur.execute("select * from Type")
-# 該資料庫 (schema)的權限必須開放select (custom)給該user
-for emp in cur.fetchall():
-    print(emp)
