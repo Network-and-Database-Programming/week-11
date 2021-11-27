50cd2e20978dbe1acafcc081b87e29771753371d
shanacon <weston123741852@gmail.com>
2021-11-27 22:08:27 +0800

    [Update] Main Feature

---
+++ Event.py
@@ -0,0 +1,221 @@
+import tkinter as tk
+import os
+from tkinter import filedialog
+import MySQLdb
+from urllib.parse import urlparse
+###
+UserName = ""
+Pswd = ""
+def NewType(Type_Input, DropDown_1, var_1, DropDown_2, var_2, StatusText) :
+    con = MySQLdb.connect(host="localhost", user = UserName, password = Pswd, db="filedata")
+    if Type_Input.get()=="":
+        StatusText['text'] = "Please input Type name"
+    else :
+        with con:
+            cur=con.cursor()
+            cur.execute(f"INSERT into filedata.Type Values(null, '{Type_Input.get()}')")
+            con.commit()
+    Type_Input.delete(0, 'end')
+    return RefreshTypeList(DropDown_1, var_1, DropDown_2, var_2)
+###
+def RefreshTypeList(DropDown_1, var_1, DropDown_2, var_2):
+    DropDown_1['menu'].delete(0, 'end')
+    DropDown_2['menu'].delete(0, 'end')
+    con = MySQLdb.connect(host="localhost", user = UserName, password = Pswd, db="filedata")
+    cur = con.cursor()
+    cur.execute("select name from Type")
+    TypeList = []
+    for item in cur.fetchall():
+        TypeList.append(item[0])
+    for item in TypeList: 
+        DropDown_1['menu'].add_command(label=item, command=tk._setit(var_1, item))
+        DropDown_2['menu'].add_command(label=item, command=tk._setit(var_2, item))
+    if TypeList :
+        var_1.set(TypeList[0])
+    return TypeList
+###
+def NewTag(Tag_Input, variable, StatusText):
+    con = MySQLdb.connect(host="localhost", user = UserName, password = Pswd, db="filedata")
+    with con:
+        cur=con.cursor()
+        if Tag_Input.get() != "":
+            if variable.get() != "":
+                cur.execute(f"SELECT type_id from Type WHERE name='{variable.get()}'")
+                id = cur.fetchone()[0]
+                cur.execute(f"INSERT into Tag Values(null, '{Tag_Input.get()}', '{id}')")
+                con.commit()
+                Tag_Input.delete(0, 'end')
+            else:
+                StatusText['text'] = "Please select Type"
+        else:
+            StatusText['text'] = "Please input Tag"
+###
+def Browse(file_path, StatusText):
+    file_path.delete(0, 'end')
+    filename = filedialog.askdirectory(initialdir = os.getcwd(),title = "Select a File")
+    if filename == None or filename == "":
+       StatusText['text'] = "please choose folder"
+       return None
+    file_path.insert(0, filename)
+    return Check_Path(filename)
+###
+def Check_Path(path) :
+    if path == "" :
+        return
+    con = MySQLdb.connect(host="localhost", user = UserName, password = Pswd, db="filedata")
+    cur=con.cursor()
+    cur.execute (f"SELECT url_id from url WHERE Urldata = '{path}'")
+    tmp = cur.fetchone()
+    if tmp == None :
+        file_id = InsertPath(path)
+    else :
+        file_id = tmp[0]
+    return file_id
+###
+def InsertPath(path):
+    con = MySQLdb.connect(host="localhost", user = UserName, password = Pswd, db="filedata")
+    with con:
+        cur=con.cursor()
+        domain = urlparse(path).netloc
+        cur.execute(f"Insert into url Values(null, '{path}', '{domain}')")
+        con.commit()
+        cur.execute(f"SELECT url_id from url WHERE Urldata = '{path}'")
+        return cur.fetchone()[0]
+###
+def SetFileTag(file_id,tag_id):
+    con = MySQLdb.connect(host="localhost", user = UserName, password = Pswd, db="filedata")
+    with con:
+        cur=con.cursor()
+        cur.execute(f"Insert into Relation Values('{file_id}', '{tag_id}')")
+        con.commit()
+##
+def RefreshTagSystem(DropDown_1, var_1, type):
+    DropDown_1['menu'].delete(0, 'end')
+    var_1.set("")
+    con = MySQLdb.connect(host="localhost", user = UserName, password = Pswd, db="filedata")
+    cur=con.cursor()
+    cur.execute (f"SELECT type_id from Type WHERE name = '{type}'")
+    type = cur.fetchone()[0]
+    TagList = []
+    cur.execute(f"SELECT name from Tag WHERE type = {type}")
+    for item in cur.fetchall():
+        TagList.append(item[0])
+    for item in TagList: 
+        DropDown_1['menu'].add_command(label=item, command=tk._setit(var_1, item))
+    if TagList :
+        var_1.set(TagList[0])
+###
+def DeleteRelation(file_id, tag_id) :
+    con = MySQLdb.connect(host="localhost", user = UserName, password = Pswd, db="filedata")
+    cur=con.cursor()
+    cur.execute(f"Delete from Relation WHERE url_id = {file_id} AND tag_id = {tag_id}")
+    con.commit()
+###
+def GetTagsByFileID(file_id) :
+    con = MySQLdb.connect(host="localhost", user = UserName, password = Pswd, db="filedata")
+    cur=con.cursor()
+    TagList = []
+    cur.execute(f"SELECT tag_id from Relation WHERE url_id = {file_id}")
+    for tag_id in cur.fetchall() :
+        TagList.append(tag_id[0])
+    return TagList
+###
+def GetPos(count, item) :
+    item.place(x = 540 + (count%2) * 130, y = 15 + int(count/2) * 35)
+def GetPosOfSearch(count, item) :
+    item.place(x = 63, y = 15 + count * 35)
+###
+def GetTagidByName(TagName, StatusText):
+    con = MySQLdb.connect(host="localhost", user = UserName, password = Pswd, db="filedata")
+    cur=con.cursor()
+    cur.execute(f"SELECT tag_id from Tag WHERE name = '{TagName}'")
+    tmp = cur.fetchone()
+    if tmp :
+        return tmp[0]
+    else :
+        StatusText['text'] = "no such tag"
+def GetFileidByPath(path, StatusText):
+    if path == None or path == "input path" :
+        StatusText['text'] = "no such file"
+        return -1
+    con = MySQLdb.connect(host="localhost", user = UserName, password = Pswd, db="filedata")
+    cur=con.cursor()
+    cur.execute (f"SELECT url_id from url WHERE Urldata = '{path}'")
+    tmp = cur.fetchone()
+    if tmp :
+        return tmp[0]
+    else :
+        StatusText['text'] = "no such file"
+def GetNameByTagid(TagID, StatusText):
+    con = MySQLdb.connect(host="localhost", user = UserName, password = Pswd, db="filedata")
+    cur=con.cursor()
+    cur.execute(f"SELECT name from Tag WHERE tag_id = {TagID}")
+    tmp = cur.fetchone()
+    if tmp :
+        return tmp[0]
+    else :
+        StatusText['text'] = "no such tag id"
+def GetPathByFileid(fileID, StatusText):
+    con = MySQLdb.connect(host="localhost", user = UserName, password = Pswd, db="filedata")
+    cur=con.cursor()
+    cur.execute (f"SELECT Urldata from url WHERE url_id = '{fileID}'")
+    tmp = cur.fetchone()
+    if tmp :
+        return tmp[0]
+    else :
+        StatusText['text'] = "no such fileid"
+####
+def SearchByString(input, StatusText) :
+    tagList = []
+    LogicList = []
+    ret = []
+    if input == None or input == "" :
+        con = MySQLdb.connect(host="localhost", user = UserName, password = Pswd, db="filedata")
+        cur=con.cursor()
+        command = "SELECT DISTINCT url_id FROM Relation"
+        cur.execute(command)
+        tmp = cur.fetchall()
+        if tmp :
+            for item in tmp :
+                ret.append(item[0])
+        else :
+            StatusText['text'] = "No file Found"
+        return ret
+    And_1 = "SELECT url_id FROM(SELECT * FROM Relation WHERE url_id IN("
+    ## succuess :"SELECT file_id FROM(SELECT * FROM Relation WHERE file_id IN(SELECT file_id FROM Relation WHERE tag_id = 8)) as a WHERE tag_id = 10"
+    tmp = input.split(' ')
+    for str in tmp :
+        if len(tagList) == len(LogicList) :
+            tagList.append(str)
+        else :
+            LogicList.append(str)
+    command = f"SELECT url_id FROM Relation WHERE tag_id = {GetTagidByName(tagList[0], StatusText)}"
+    count = 1
+    for logic in LogicList :
+        if logic == "and":
+            command = And_1 + command + f")) as a WHERE tag_id = {GetTagidByName(tagList[count], StatusText)}"
+        elif logic == "or":
+            orcmd = f"SELECT url_id FROM Relation WHERE tag_id = {GetTagidByName(tagList[count], StatusText)}"
+            command = command + " UNION " + orcmd
+        count+=1
+    con = MySQLdb.connect(host="localhost", user = UserName, password = Pswd, db="filedata")
+    cur=con.cursor()
+    cur.execute(command)
+    tmp = cur.fetchall()
+    if tmp :
+        for item in tmp :
+            ret.append(item[0])
+    else :
+        StatusText['text'] = "No file Found"
+    return ret
+###
+def Login(username, psw):
+    global UserName
+    global Pswd
+    try :
+        con = MySQLdb.connect(host="localhost", user = username, password = psw, db = "filedata")
+        UserName = username
+        Pswd = psw
+        return True
+    except :
+        return False
---
+++ main.py
@@ -0,0 +1,152 @@
+import tkinter as tk
+import tkinter.font as font
+from Event import *
+window = tk.Tk()
+###
+BtnFont = font.Font(size=20)
+EntryFont = font.Font(size=14)
+###
+TypeList = []
+TagList = []
+TagBtnList = []
+Now_fileID = -1
+login_status = False
+### function
+def AddTagByDropDown() :
+    tag_id = GetTagidByName(tag_var.get(), StatusText)
+    file_id = GetFileidByPath(file_path.get(), StatusText)
+    if file_id != -1 :
+        SetFileTag(file_id, tag_id)
+        SetTagsShow()
+def AddTagByEntry() :
+    tag_id = GetTagidByName(Tag_E.get(), StatusText)
+    file_id = GetFileidByPath(file_path.get(), StatusText)
+    Tag_E.delete(0, 'end')
+    if file_id != -1 :
+        SetFileTag(file_id, tag_id)
+        SetTagsShow()
+def SetTagsShow() :
+    RemoveAllTagBtn()
+    FileTagList = GetTagsByFileID(Now_fileID)
+    count = 0
+    for id in FileTagList :
+        newBtn = tk.Button(window, text = GetNameByTagid(id, StatusText), bg = "antique white", width = '10', height = '1', font = EntryFont)
+        newBtn.config(command = lambda id = id :_DeleteRelation(Now_fileID, id))
+        GetPos(count, newBtn)
+        TagBtnList.append(newBtn)
+        count += 1
+def RemoveAllTagBtn() :
+    global TagBtnList
+    for Btn in TagBtnList :
+        Btn.destroy()
+    TagBtnList.clear()
+def SetSearchResultWindow(PathList) :
+    SearchResult = tk.Toplevel(window)
+    SearchResult.geometry('400x500')
+    ###
+    count = 0
+    for path in PathList :
+        newBtn = tk.Button(SearchResult, text = path, bg = "antique white", width = '25', height = '1', font = EntryFont)
+        newBtn.config(command = lambda path = path :OpenExploer(path))
+        GetPosOfSearch(count, newBtn)
+        count += 1
+def OpenExploer(path):
+    os.startfile(path)
+def _NewTag() :
+    NewTag(Tag_Input, variable, StatusText)
+    RefreshTagSystem(tag_d, tag_var, variable.get())
+# def _Browse() :
+#     global Now_fileID
+#     Now_fileID = Browse(file_path, StatusText)
+#     SetTagsShow()
+def _Check_Path() :
+    global Now_fileID
+    Now_fileID = Check_Path(file_path.get())
+    SetTagsShow()
+def _DeleteRelation(file_id, tag_id) :
+    DeleteRelation(file_id, tag_id)
+    SetTagsShow()
+def _SearchByString() :
+    FileidList = SearchByString(Search_Input.get(), StatusText)
+    PathList = []
+    for id in FileidList :
+        PathList.append(GetPathByFileid(id, StatusText))
+    SetSearchResultWindow(PathList)
+def _Login():
+    if Login(User_Input.get(), PSW_Input.get()) :
+        showUI()
+        StatusText['text'] = ""
+    else :
+        StatusText['text'] = "login error"
+### Label
+StatusText = tk.Label(window, text ="", font = EntryFont)
+StatusText.place(x = 380, y = 60)
+### button
+New_Type_Btn = tk.Button(window, text ="New Type", bg = "light blue", width = '10', height = '1', font = BtnFont)
+New_Tag_Btn = tk.Button(window, text ="New Tag", bg = "light blue", width = '10', height = '1', font = BtnFont)
+# Browse_Btn = tk.Button(window, text ="Browse", bg = "light blue", width = '10', height = '1', font = BtnFont)
+# Browse_Btn.place(x = 50, y = 175)
+Add_Tag_Btn = tk.Button(window, text ="Add Tag", bg = "light blue", width = '10', height = '1', font = BtnFont)
+Check_Btn = tk.Button(window, text ="Check", bg = "light blue", width = '10', height = '1', font = EntryFont)
+Add_Tag_Btn2 = tk.Button(window, text ="Add Tag", bg = "light blue", width = '10', height = '1', font = EntryFont)
+Search_Btn = tk.Button(window, text ="Search", bg = "light blue", width = '10', height = '1', font = BtnFont)
+##
+Login_Btn = tk.Button(window, text ="Login", bg = "light blue", width = '10', height = '1', font = BtnFont)
+Login_Btn.place(x = 550, y = 105)
+### input entry
+Type_Input = tk.Entry(window, font = BtnFont, width = '11')
+Tag_Input = tk.Entry(window, font = BtnFont, width = '11')
+file_path = tk.Entry(window, font = EntryFont, width = '38')
+file_path.insert(0, 'input url')
+Tag_E = tk.Entry(window, font = EntryFont, width = '16')
+Tag_E.insert(0, 'input tag')
+Search_Input = tk.Entry(window, font = EntryFont, width = '34')
+#
+User_Input = tk.Entry(window, font = EntryFont, width = '20')
+User_Input.insert(0, 'input username')
+User_Input.place(x = 550, y = 50)
+PSW_Input = tk.Entry(window, font = EntryFont, width = '20', show="*")
+PSW_Input.place(x = 550, y = 80)
+### dropdown
+variable = tk.StringVar(window)
+type_of_tag = tk.OptionMenu(window, variable, "")
+type_of_tag.config(width = 11, bg = "light blue", font = EntryFont)
+#
+type_var = tk.StringVar(window)
+type_d = tk.OptionMenu(window, type_var, "")
+type_d.config(width = 12, bg = "light blue", font = EntryFont)
+tag_var = tk.StringVar(window)
+tag_d = tk.OptionMenu(window, tag_var, "")
+tag_d.config(width = 12, bg = "light blue", font = EntryFont)
+### set command
+New_Type_Btn.config(command = lambda:NewType(Type_Input, type_of_tag, variable, type_d, type_var, StatusText))
+New_Tag_Btn.config(command = lambda:_NewTag())
+# Browse_Btn.config(command = lambda:_Browse())
+Check_Btn.config(command = lambda:_Check_Path())
+type_var.trace("w", lambda *args: RefreshTagSystem(tag_d, tag_var, type_var.get()))
+Add_Tag_Btn.config(command = lambda:AddTagByDropDown())
+Add_Tag_Btn2.config(command = lambda:AddTagByEntry())
+Search_Btn.config(command = lambda:_SearchByString())
+Login_Btn.config(command = lambda:_Login())
+def showUI():
+    global TypeList  
+    TypeList = RefreshTypeList(type_of_tag, variable, type_d, type_var)
+    New_Type_Btn.place(x = 50, y = 50)
+    New_Tag_Btn.place(x = 50, y = 100)
+    Add_Tag_Btn.place(x = 370, y = 190)
+    Check_Btn.place(x = 410, y = 142)
+    Add_Tag_Btn2.place(x = 370, y = 260)
+    Search_Btn.place(x = 370, y = 325)
+    Type_Input.place(x = 210, y = 58)
+    Tag_Input.place(x = 210, y = 108)
+    file_path.place(x = 50, y = 150)
+    Tag_E.place(x = 210, y = 265)
+    Search_Input.place(x = 50, y = 337)
+    type_of_tag.place(x = 378, y = 108)
+    type_d.place(x = 208, y = 175)
+    tag_d.place(x = 208, y = 215)
+###
+window.title('UrlTagger')
+window.geometry('800x400')
+window.mainloop()
+###
